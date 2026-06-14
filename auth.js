2b9d503bb45c21fd3821d08c66c93b9cf58f8f23
Amam17 <dauramansurahmad83@gmail.com>
2026-06-14 16:37:17 +0100

    Delete auth.js

--- auth.js
+++
@@ -1,6 +0,0 @@
-import { auth } from "./firebase-config.js";
-
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
