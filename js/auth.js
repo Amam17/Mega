531594dd3ab4c12133ff29e9be71361d11507c7e
Amam17 <dauramansurahmad83@gmail.com>
2026-06-14 18:23:09 +0100

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,6 @@
+import { auth } from "./firebase-config.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
