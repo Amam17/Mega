e6e3048685d3118b300c1ccfdf71f6044b8812fd
Amam17 <dauramansurahmad83@gmail.com>
2026-06-15 10:25:27 +0100

    Refactor Firebase config and initialization code

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,34 +1,38 @@
 // Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { initializeFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnFW4v6GLdDyCsgMwoySyss4T4Q93hVtQ",
-  authDomain: "meganet-5be7c.firebaseapp.com",
-  projectId: "meganet-5be7c",
-  storageBucket: "meganet-5be7c.firebasestorage.app",
-  messagingSenderId: "85379992133",
-  appId: "1:85379992133:web:8c029bc68389bd2313312c",
-  measurementId: "G-N3HMR7QQ90"
+apiKey:"AIzaSyAnFW4v6GLdDyCsgMwoySyss4T4Q93hVtQ",
+authDomain:"meganet-5be7c.firebaseapp.com",
+projectId:"meganet-5be7c",
+storageBucket:"meganet-5be7c.firebasestorage.app",
+messagingSenderId:"85379992133",
+appId:"1:85379992133:web:8c029bc68389bd2313312c",
+measurementId:"G-N3HMR7QQ90"
 };
 
 
-// Initialize Firebase
+// Initialize App
 const app = initializeApp(firebaseConfig);
 
 
 // Export Services
 export const auth = getAuth(app);
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true
-});
+
+export const db = getFirestore(app);
+
 export const storage = getStorage(app);
 
 export default app;
