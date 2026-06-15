69da30c461a16e08b451cd7cdcd849a59cf49f78
Amam17 <dauramansurahmad83@gmail.com>
2026-06-15 11:38:31 +0100

    Refactor Firebase config and imports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,10 +7,7 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { getStorage }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
-
-const firebaseConfig={
+const firebaseConfig = {
 
 apiKey:"AIzaSyAnFW4v6GLdDyCsgMwoySyss4T4Q93hVtQ",
 
@@ -18,24 +15,17 @@ authDomain:"meganet-5be7c.firebaseapp.com",
 
 projectId:"meganet-5be7c",
 
-storageBucket:"meganet-5be7c.firebasestorage.app",
-
 messagingSenderId:"85379992133",
 
 appId:"1:85379992133:web:8c029bc68389bd2313312c"
 
 };
 
-const app=
+const app =
 initializeApp(firebaseConfig);
 
-export const auth=
+export const auth =
 getAuth(app);
 
-export const db=
+export const db =
 getFirestore(app);
-
-export const storage=
-getStorage(app);
-
-export default app;
